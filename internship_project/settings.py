2323602f199fd62f2123e25f8b2495d70587308f
Tanushree2004 <tanshreejati2004@gmail.com>
2025-09-23 13:26:05 +0530

    Update ALLOWED_HOSTS for Render deployment

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['internship-django.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
